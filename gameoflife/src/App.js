4246386ed0c5687d3f6659dfb56f3e5aa57d07fa
Oscar Valenzuela <oscar.valenzuela69@gmail.com>
2020-06-18 19:42:42 -0700

    setting up grid

--- gameoflife/src/App.js
+++ gameoflife/src/App.js
@@ -3,9 +3,29 @@ import './App.css';
 
 class Grid extends React.Component {
   render(){
+    const width = this.props.cols * 14;
+    let rowsArr = [];
+    let boxClass = '';
+    for (let i = 0 ; i < this.props.rows; i++){
+      for (let j = 0 ; j < this.props.rows; j++){
+        let boxId = i + "_" + j;
+
+        boxClass = this.props.gridFull[i][j] ? 'box on': 'box off';
+        rowsArr.push(
+          <Box
+          boxClass = {boxClass}
+          key={boxId}
+          boxid={boxId}
+          row={i}
+          col={j}
+          selectBox={this.props.selectBox}
+          />
+        )
+      }
+    }
     return (
-      <div>
-        Grid
+      <div className="grid" style={{width:width}}>
+       {{rowsArr}}
       </div>
     )
   }
@@ -14,15 +34,24 @@ class Grid extends React.Component {
 class App extends React.Component {
   constructor(){
     super();
+    this.speed = 100;
+    this.rows = 30;
+    this.cols = 50;
     this.state = {
       generation: 0,
+      gridFull : Array(this.rows).fill().map(() => Array(this.cols).fill(false))
     }
   }
   render(){
     return (
       <div>
       <h1>The Game of Life</h1>
-      <Grid/>
+      <Grid
+      gridFull={this.state.gridFull}
+      rows={this.rows}
+      cols={this.cols}
+      selectBox = {this.selectBox}
+      />
       <h2>Generations: {this.state.generation}</h2>
       </div>
     )
